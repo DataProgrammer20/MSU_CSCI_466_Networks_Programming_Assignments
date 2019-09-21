311e92ab78ce73cbe8a8d9fa7a1560e9fdfe89e6
William <William@BEASTMACHINE>
2019-09-21 14:09:35 -0600

    Updating client.py

--- assignment_1/client.py
+++ assignment_1/client.py
@@ -1,4 +1,4 @@
-from socket import *
+from http.client import *
 import sys as system
 
 
@@ -10,29 +10,16 @@ class BattleshipClient:
     client_socket = ''
 
     def __init__(self, ip="127.0.0.1", port=5000, x=0, y=0):
-
-        # Debugging values
-        print("Command line values: ", ip, port, x, y)
-        # ================
-
         self.ip = ip
         self.port = int(port)
         self.x = int(x)
         self.y = int(y)
-        self.client_socket = socket(AF_INET, SOCK_STREAM)
-        self.client_socket.connect((self.ip, self.port))
 
-        command = 'fire ' + str(self.x) + ' ' + str(self.y)
-
-        # sending message
-        self.client_socket.send(command.encode())
-
-        # response
-        modified_sentence = self.client_socket.recv(1024)
-
-        print('Server Response: ', modified_sentence.decode())
-
-        self.client_socket.close()
+        connection = HTTPConnection(self.ip, self.port)
+        print(connection)
+        connection.request("GET", "/")
+        response = connection.getresponse()
+        print("Status: ", response.status)
 
 client = BattleshipClient(
     system.argv[1],
