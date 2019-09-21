1dd77b817e7f1020feeeeeec45da5ef6f51eb68c
William <William@BEASTMACHINE>
2019-09-21 11:49:43 -0600

    Updating client.py and txt files

--- assignment_1/client.py
+++ assignment_1/client.py
@@ -1,10 +1,35 @@
 from socket import *
-server_name = 'localhost'
-server_port = 6000
-client_socket = socket(AF_INET, SOCK_STREAM)
-client_socket.connect((server_name, server_port))
-sentence = 'input lowercase sentence'
-client_socket.send(sentence.encode())
-modifiedSentence = client_socket.recv(1024)
-print('Server Response: ', modifiedSentence.decode())
-client_socket.close()
+import sys as system
+
+
+class BattleshipClient:
+    ip = 0
+    port = 0
+    x = 0
+    y = 0
+    client_socket = ''
+
+    def __init__(self, ip="127.0.0.1", port=6000, x=0, y=0):
+
+        # Debugging values
+        print("Command line values: ", ip, port, x, y)
+        # ================
+
+        self.ip = ip
+        self.port = port
+        self.x = x
+        self.y = y
+        self.client_socket = socket(AF_INET, SOCK_STREAM)
+        self.client_socket.connect((self.ip, self.port))
+        sentence = 'hello from client town'
+        self.client_socket.send(sentence.encode())
+        modified_sentence = self.client_socket.recv(1024)
+        print('Server Response: ', modified_sentence.decode())
+        self.client_socket.close()
+
+client = BattleshipClient(
+    system.argv[1],
+    system.argv[2],
+    system.argv[3],
+    system.argv[4]
+)
