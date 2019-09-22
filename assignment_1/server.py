fb17f4d5ee6017b05066def552ad005e1b605e87
William <William@BEASTMACHINE>
2019-09-22 11:37:32 -0600

    Updating game

--- assignment_1/server.py
+++ assignment_1/server.py
@@ -7,6 +7,7 @@ from http.server import *
 class BattleshipServer(BaseHTTPRequestHandler):
     port = 0
     client_board_file = ''
+    server_board_file = 'opponent_board.txt'
     client_ship_count = 5
     server_ship_count = 5
     server_victory = False
@@ -14,26 +15,60 @@ class BattleshipServer(BaseHTTPRequestHandler):
     server_tokens = {'C': 5, 'B': 4, 'R': 3, 'S': 3, 'D': 2}
     client_tokens = {'C': 5, 'B': 4, 'R': 3, 'S': 3, 'D': 2}
     client_board = []
-    server_board = [
-        ['_', 'C', '_', '_', '_', '_', '_', '_', '_', '_'],
-        ['_', 'C', '_', '_', '_', '_', '_', '_', 'S', '_'],
-        ['_', 'C', '_', '_', '_', '_', '_', '_', 'S', '_'],
-        ['_', 'C', '_', 'B', 'B', 'B', 'B', '_', 'S', '_'],
-        ['_', 'C', '_', '_', '_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_', '_', 'D', '_', '_'],
-        ['_', 'R', 'R', 'R', '_', '_', '_', 'D', '_', '_'],
-        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_'],
-        ['_', '_', '_', '_', '_', '_', '_', '_', '_', '_']
-    ]
+    server_board = []
 
     # def server_turn(self):
-    #
+
+    def get_server_file_content(self):
+        file = open('opponent_board.txt', 'r')
+        character = file.readline(1)
+        line = ''
+        file_content = ''
+        char_count = 0
+        while character:
+            if char_count == 10:
+                file_content += line + '<br>'
+                line = ''
+                char_count = 0
+            if (character == 'C' or character == 'R' or character == 'D' or character == 'B' or character == 'S'):
+                line += '_'
+                character = file.readline(1)
+                char_count += 1
+            elif character == '\n':
+                character = file.readline(1)
+                continue
+            else:
+                line += character
+                character = file.readline(1)
+                char_count += 1
+        return file_content
+
+    def get_client_file_content(self):
+        file = open('own_board.txt', 'r')
+        line = file.readline()
+        file_content = ''
+        while line:
+            file_content += line + '<br>'
+            line = file.readline()
+        return file_content
+
+    def save_to_file(self, type, board):
+        file = ''
+        if type == 'client':
+            file = self.client_board_file
+        else:
+            file = self.server_board_file
+        open(file, 'w').close()
+        file_append = open(file, 'a')
+        line = ''
+        for row in board:
+            for character in row:
+                line += character
+            file_append.write(line + '\n')
+            line = ''
+        file_append.close()
 
     def check_victory(self, ship_count, player):
-
-        print(self.server_ship_count)
-
         result = [False, '']
         if ship_count == 0:
             result[0] = True
@@ -47,18 +82,30 @@ class BattleshipServer(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(bytes(message, "utf8"))
 
-    def parse_client_board(self):
-        client_file = open(self.client_board_file, 'r')
+    def parse_board(self, type):
         board_line = []
-        file_line = client_file.readline()
-        while file_line:
-            for index in range(0, 10):
-                character = file_line[index]
-                board_line.append(character)
-            self.client_board.append(board_line)
-            board_line = []
+        if type == 'client':
+            client_file = open(self.client_board_file, 'r')
             file_line = client_file.readline()
-        client_file.close()
+            while file_line:
+                for index in range(0, 10):
+                    character = file_line[index]
+                    board_line.append(character)
+                self.client_board.append(board_line)
+                board_line = []
+                file_line = client_file.readline()
+            client_file.close()
+        else:
+            server_file = open(self.server_board_file, 'r')
+            file_line = server_file.readline()
+            while file_line:
+                for index in range(0, 10):
+                    character = file_line[index]
+                    board_line.append(character)
+                self.server_board.append(board_line)
+                board_line = []
+                file_line = server_file.readline()
+            server_file.close()
 
     @classmethod
     def run(cls, port=5000, client_board_file=''):
@@ -69,21 +116,41 @@ class BattleshipServer(BaseHTTPRequestHandler):
         httpd = HTTPServer(server_address, BattleshipServer)
         print('Server is listening on IP: 127.0.0.1, ' + 'port: ' + str(cls.port))
         print('Parsing client board...')
-        cls.parse_client_board(cls)
+        cls.parse_board(cls, 'client')
+        print('Parsing server board...')
+        cls.parse_board(cls, 'server')
         print('Awaiting client connections...')
         httpd.serve_forever()
 
     def do_GET(self):
-        # Send response status code
-        self.send_response(200)
-        # Send headers
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
-        # Send message back to client
-        message = "Welcome to the Battleship server!"
-        # Write content as utf-8 data
-        self.wfile.write(bytes(message, "utf8"))
-        return
+        if self.path == '/own_board.html':
+            file_content = self.get_client_file_content()
+            DOM = '<div style="margin: 100px auto; text-align: left; width: 100px;">' + file_content + '</div>'
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(DOM, 'utf8'))
+            return
+
+        elif self.path == '/opponent_board.html':
+            file_content = self.get_server_file_content()
+            DOM = '<div style="margin: 100px auto; text-align: left; width: 100px;">' + file_content + '</div>'
+            self.send_response(200)
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            self.wfile.write(bytes(DOM, 'utf8'))
+            return
+
+        else:
+            self.send_response(200)
+            # Send headers
+            self.send_header('Content-type', 'text/html')
+            self.end_headers()
+            # Send message back to client
+            message = "Welcome to the Battleship server!"
+            # Write content as utf-8 data
+            self.wfile.write(bytes(message, 'utf8'))
+            return
 
     def do_POST(self):
         status = 0
@@ -94,7 +161,7 @@ class BattleshipServer(BaseHTTPRequestHandler):
         parsed_url = urlparse.urlparse(url)
         y_coord = int(urlparse.parse_qs(parsed_url.query)['y'][0])
         x_coord = int(urlparse.parse_qs(parsed_url.query)['x'][0])
-        game_over = self.check_victory(self.server_ship_count, self.server_tokens, 'client')
+        game_over = self.check_victory(self.server_ship_count, 'client')
 
         if game_over[0]:
             status = 200
@@ -140,8 +207,8 @@ class BattleshipServer(BaseHTTPRequestHandler):
             headers.append('127.0.0.1:5000')
             message = 'Unknown command'
 
-        print(self.server_ship_count)
         self.send_http_response(status, headers, message)
+        self.save_to_file('server', self.server_board)
         return
 
 BattleshipServer.run(system.argv[1], system.argv[2])
